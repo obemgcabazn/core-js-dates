6e3553e1a2220f953a7440ed8deb0bdfdc520a7c
Electropompa <admin@zilmet.ru>
2025-03-24 17:32:43 +0300

    feat: getNextFridayThe13th

--- src/core-js-dates-tasks.js
+++ src/core-js-dates-tasks.js
@@ -224,8 +224,17 @@ function getWeekNumberByDate(date) {
  * Date(2024, 0, 13) => Date(2024, 8, 13)
  * Date(2023, 1, 1) => Date(2023, 9, 13)
  */
-function getNextFridayThe13th(/* date */) {
-  throw new Error('Not implemented');
+function getNextFridayThe13th(date) {
+  const currentDate = new Date(date);
+  currentDate.setHours(0, 0, 0, 0);
+
+  currentDate.setDate(13);
+
+  while (currentDate.getDay() !== 5) {
+    currentDate.setMonth(currentDate.getMonth() + 1);
+    currentDate.setDate(13);
+  }
+  return currentDate;
 }
 
 /**
